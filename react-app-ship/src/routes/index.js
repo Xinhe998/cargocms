4c8c47b23cb7dd13230b4e5ccd7e67871b4d6c75
Kuyen <iamcxa@gmail.com>
2017-01-10 16:49:39 +0800

    add check when user is not login

--- react-app-ship/src/routes/index.js
+++ react-app-ship/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import Lang from 'lodash';
 
 // NOTE: here we're making use of the `resolve.root` configuration
 // option in webpack, which allows us to specify import paths as if
@@ -12,11 +13,19 @@ import Login from 'views/Login/Login';
 import MaterialUi from 'views/MaterialUi/MaterialUi';
 import ShipList from 'views/ShipListView';
 
+function redirectToLogin(props, replace) {
+  // if (Lang.isUndefined(props.auth)) {
+  //   replace({
+  //     pathname: '/ship/login',
+  //   });
+  // }
+}
+
 export default (store) => (
   <Route path='/'>
     <IndexRoute component={Login} />
     <Route path='/ship/login' component={Login} />
-    <Route path='/ship' component={CoreLayout}>
+    <Route path='/ship' component={CoreLayout} onEnter={redirectToLogin}>
       <IndexRoute component={ShipList} />
       <Route path='/ship/history' component={ShipList} />
       <Route path='/ship/user' component={ShipList} />
