52c6b8078fa6d227470e1661dbf085af31fce4bc
Kuyen <iamcxa@gmail.com>
2017-01-10 16:49:12 +0800

    add request error handler

--- react-app-ship/src/redux/rootReducer.js
+++ react-app-ship/src/redux/rootReducer.js
@@ -3,8 +3,10 @@ import { routerReducer as router } from 'react-router-redux';
 import counter from './modules/counter';
 import shipOrder from './modules/shipOrder';
 import toast from './modules/toast';
+import error from './modules/errorHandler';
 
 export default combineReducers({
+  error,
   shipOrder,
   counter,
   toast,
