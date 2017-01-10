b384537703b293098527a465cc2e11650bb9a2e2
smlsunxie <smlsun@gmail.com>
2017-01-10 09:28:14 +0800

    完成 config 載入指定模組

--- config/util/customConfigLoader.js
+++ config/util/customConfigLoader.js
@@ -1,13 +1,20 @@
 import fs from 'fs';
+import rc from 'rc';
 export default (configName) => {
+  let rcConfig = rc('sails');
+  let {modules} = rcConfig.configLoader
+  console.log("modules", modules);
+
   var result = {}
   var files = fs.readdirSync('./config')
   for (var dirName of files) {
     let isDir = fs.statSync('./config/' + dirName).isDirectory();
-    if (isDir) {
+    if (isDir && modules.indexOf(dirName) >= 0) {
       let customRouteConfigFile = __dirname+"/../" + dirName + '/'+configName;
+
       let hasCustomRouteConfigFile = fs.existsSync(customRouteConfigFile);
       if (hasCustomRouteConfigFile) {
+        console.log("load module", dirName, configName);
         var customConfig = require(__dirname+"/../" + dirName + '/'+configName);
 
         result = {
