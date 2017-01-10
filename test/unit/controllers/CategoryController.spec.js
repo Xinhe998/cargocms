af920f3e41b9802e8cb16290819bad468cd8fb56
smlsunxie <smlsun@gmail.com>
2017-01-10 18:47:54 +0800

    新增 sub Category

--- test/unit/controllers/CategoryController.spec.js
+++ test/unit/controllers/CategoryController.spec.js
@@ -5,7 +5,7 @@ describe.skip('about category Controller operation.', function() {
   it('get category list by parentId should be success.', async (done) => {
     try {
 
-      request(sails.hooks.http.app).get(`/api/category?parentId=1`)
+      request(sails.hooks.http.app).get(`/api/category`)
 
       /* results
       {
@@ -21,7 +21,18 @@ describe.skip('about category Controller operation.', function() {
              metaDescription: 'meta desc 1',
              metaKeyword: 'meta,keyword,test,1',
           },
-         }]
+          SubCategory: [{
+            sortOrder: 1,
+            ParentId: 1,
+            CategoryDescription:{
+               name: 'categoryData1',
+               description: 'test desc 1',
+               metaTitle: 'meta title 1',
+               metaDescription: 'meta desc 1',
+               metaKeyword: 'meta,keyword,test,1',
+             }
+           }],
+         }],
       }
       */
 
