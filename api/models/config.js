8ae5225cd25f2b42d37677ad7f5e57b745b39e2f
smlsunxie <smlsun@gmail.com>
2017-01-11 09:18:12 +0800

    新增 config model

---
+++ api/models/config.js
@@ -0,0 +1,57 @@
+import moment from 'moment';
+
+module.exports = {
+  attributes: {
+    name: {
+      type:  Sequelize.STRING,
+      allowNull: false,
+    },
+    key: {
+      type:  Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type:  Sequelize.STRING,
+      allowNull: false,
+    },
+    value: {
+      type:  Sequelize.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: Sequelize.ENUM('text', 'editor', 'url', 'file'),
+      defaultValue: 'text',
+      allowNull: false,
+    },
+
+    createdDateTime: {
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('createdAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    },
+    updatedDateTime: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('updatedAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    },
+  },
+  associations: function() {
+
+  },
+  options: {
+    paranoid: true,
+    classMethods: {},
+    instanceMethods: {},
+    hooks: {}
+  }
+};
