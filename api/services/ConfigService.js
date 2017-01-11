9217462d0cf09d6d25dac7fa182473bd82eeb7af
smlsunxie <smlsun@gmail.com>
2017-01-11 09:29:21 +0800

    新增 ConfigService 與 spec

---
+++ api/services/ConfigService.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  sync: function() {
+
+  },
+
+  load: function() {
+
+  },
+
+  init: function() {
+
+  }
+
+}
---
+++ test/unit/services/ConfigService.spec.js
@@ -0,0 +1,38 @@
+describe.skip('about Config Service operation.', function() {
+  it('將 sails.config 同步至 config model', async (done) => {
+
+    try {
+      let result = ConfigService.sync();
+      result.should.be.true;
+      done();
+    } catch (e) {
+      done(e);
+    }
+
+  });
+
+  it('config model 更新後，重新載入 sails.fonfig', async (done) => {
+
+    try {
+      let result = ConfigService.load();
+      result.should.be.true;
+      done();
+    } catch (e) {
+      done(e);
+    }
+
+  });
+
+  it('相關 service 重新初始，如：email, passport', async (done) => {
+
+    try {
+      let result = ConfigService.init();
+      result.should.be.true;
+      done();
+    } catch (e) {
+      done(e);
+    }
+
+  });
+
+});
