1df4c09401f63eafefaad6db4d4be7fc54a65c2c
smlsunxie <smlsun@gmail.com>
2017-01-12 17:32:05 +0800

    新增 Role 考慮 api 控管

--- api/models/RoleDetail.js
+++ api/models/RoleDetail.js
@@ -3,7 +3,11 @@ module.exports = {
     name: {
       type: Sequelize.ENUM("READ_WRITE", "READ", "CREATE", "UPDATE", "DELETE"),
       defaultValue: 'READ_WRITE',
+    },
+    api: {
+      type: Sequelize.STRING
     }
+
   },
 
   associations: function() {
