d4eb05a5b14184be66eb211b23fa95fe281cd8ac
smlsunxie <smlsun@gmail.com>
2017-01-13 13:55:58 +0800

    復原 lfp 功能清單資料初始

--- config/init/labfnp/other/index.js
+++ config/init/labfnp/other/index.js
@@ -14,10 +14,6 @@ module.exports.init = async () => {
       { icon: 'home', href: '/admin/dashboard', title: '控制台', sequence: 0},
       { icon: 'wrench', href: '#', title: '資料維護', sequence: 1},
       { icon: 'puzzle-piece', href: '#', title: '實驗室', sequence: 2},
-      { icon: 'product-hunt', href: '#', title: '產品管理', sequence: 3},
-      { icon: 'file-text', href: '#', title: '訂單管理', sequence: 4},
-      { icon: 'truck', href: '#', title: '供應商', sequence: 5},
-
 
       { href: '/admin/user', title: '會員資料', sequence: 20, ParentMenuItemId: 2},
       { href: '/admin/post', title: '內容資料', sequence: 30, ParentMenuItemId: 2},
@@ -35,21 +31,8 @@ module.exports.init = async () => {
       { href: '/admin/contact', title: '聯繫訊息', sequence: 150, ParentMenuItemId: 2},
       { href: '/admin/mock', title: '隨機資料表', sequence: 20, ParentMenuItemId: 3},
 
-      { href: '/admin/product', title: '產品', sequence: 20, ParentMenuItemId: 4},
-      { href: '/admin/productdescription', title: '產品描述', sequence: 30, ParentMenuItemId: 4},
-
-      { href: '/admin/order', title: '訂單', sequence: 20, ParentMenuItemId: 5},
-      { href: '/admin/orderproduct', title: '產品訂單', sequence: 30, ParentMenuItemId: 5},
-      { href: '/admin/orderpayment', title: '訂單付款資訊', sequence: 40, ParentMenuItemId: 5},
-      { href: '/admin/orderpaymentstatus', title: '付款狀態', sequence: 50, ParentMenuItemId: 5},
-      { href: '/admin/orderpaymenthistory', title: '訂單付款紀錄', sequence: 60, ParentMenuItemId: 5},
-
-
-      { href: '/admin/supplier', title: '供應商清單', sequence: 20, ParentMenuItemId: 6},
-      { href: '/admin/suppliershiporder', title: '供應商出貨訂單', sequence: 30, ParentMenuItemId: 6},
-      { href: '/admin/suppliershiporderproduct', title: '供應商詳細出貨訂單', sequence: 40, ParentMenuItemId: 6},
-
     ]
+
 
     let title = newMenuItems.map(item => item.title)
     let findMenuItems = await MenuItem.findAll({where:{title}})
