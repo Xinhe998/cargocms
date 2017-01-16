8a18e1b75c2cbedaa930817910c15ccb661f1273
Kuyen <iamcxa@gmail.com>
2017-01-16 16:03:41 +0800

    implement member enterance loader

---
+++ assets-motorworld/js/divBuilder.js
@@ -0,0 +1,33 @@
+var mainUrl = 'http://localhost:5001';
+var divId = '#member';
+var urlParams = '';
+var height = 30;
+
+function getEntrance() {
+  console.log('urlParams=>', window.location);
+  urlParams = window.location.search || '?debug=false';
+  var hasDebug = urlParams.split('?')[1].split('=')[0] == 'debug';
+  var isDebug = hasDebug && urlParams.split('?')[1].split('=')[1] == 'true';
+  console.log('[Merlin] : should I magically show the entrance? =>', isDebug);
+
+  if(isDebug) {
+    iFrameTarget = mainUrl + '/entrance';
+    iFrameDiv ='<iframe src=' + iFrameTarget + ' ' +
+      'frameborder="0" scrolling="no" name="member" height=' + height + '></iframe>';
+    try {
+      setStyle();
+      $(divId).append(iFrameDiv);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+function setStyle() {
+  $(divId).css('right', '120px');
+  $(divId).css('width', '200px');
+  $(divId).css('position', 'absolute');
+  $(divId).css('margin-top', '80px');
+  $(divId).css('height', height + 'px');
+  $('div#top-field1').css('padding-bottom', '25px');
+}
---
+++ assets-motorworld/js/getEnterance.js
@@ -0,0 +1,50 @@
+var mainUrl = 'http://localhost:5001';
+//----------------------------------------------------------------------------/
+//------------------------------- main part ----------------------------------//
+//----------------------------------------------------------------------------//
+javascript: (function(e, s) {
+    e.src = s;
+    e.onload = function() {
+        jQuery.noConflict();
+        console.log('jQuery injected');
+
+        $ = jQuery;
+        refuel();
+        console.log("[Merlin] : refuel!");
+    };
+    document.head.appendChild(e);
+})(document.createElement('script'), '//code.jquery.com/jquery-latest.min.js');
+
+//-------------------------------- refuel ------------------------------------//
+function refuel() {
+
+  if($('div#member').length == 0) {
+    $('div#top-field1').append('<div id="member"></div>') ;
+  }
+
+  var jsUrl = mainUrl + "/assets/motorworld/js/postscribe.min.js";
+
+  $.getScript(jsUrl)
+    .done(function(script, textStatus) {
+      launch();
+      console.log("[Merlin] : launch!");
+    }) // end done
+    .fail(function(jqxhr, settings, exception) {
+      console.log("[Merlin] : Load postscribe failed. :(");
+    }); // end fail
+
+} // end refuel
+
+//-------------------------------- launch ------------------------------------//
+function launch() {
+
+  var jsUrl = mainUrl + "/assets/motorworld/js/divBuilder.js";
+  $.getScript(jsUrl)
+    .done(function(script, textStatus) {
+      getEntrance();
+    }) // end success
+    .fail(function(jqxhr, settings, exception) {
+      console.log("[Merlin] : Load divBuilder failed.:(");
+    }); // end fail
+
+}; // end launch
