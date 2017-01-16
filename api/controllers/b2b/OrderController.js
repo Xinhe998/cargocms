ced162225fc79484f4f74abee8aebd2f56931850
KenYA <tikka0910@gmail.com>
2017-01-16 15:26:21 +0800

    token 改使用 utilsService

--- api/controllers/b2b/OrderController.js
+++ api/controllers/b2b/OrderController.js
@@ -1,15 +1,11 @@
-import crypto from 'crypto';
-
 module.exports = {
   orderForm: async (req, res) => {
-
-    const token = crypto.randomBytes(32).toString('hex').substr(0, 32);
-
+    const token = await UtilsService.tokenGenerator();
     res.view(
       'b2b/order/form',
       {
         token
       }
     );
-  }
+  },
 }
