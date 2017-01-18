2a147c6685db7a172c82d60c8dcd67f234b9358a
KenYa <tikka0910@gmail.com>
2017-01-18 17:29:08 +0800

    將不會變動的變數，改為 const

--- api/controllers/api/admin/OrderHistoryController.js
+++ api/controllers/api/admin/OrderHistoryController.js
@@ -5,7 +5,7 @@ module.exports = {
       const { query, method, body } = req;
       const { serverSidePaging } = query;
       const modelName = req.options.controller.split("/").reverse()[0];
-      const include = [  ];
+      const include = [];
       const isPost = method === 'POST';
       let mServerSidePaging = isPost ? body.serverSidePaging : serverSidePaging;
       let mQuery = isPost ? body : query;
@@ -41,9 +41,9 @@ module.exports = {
 
   create: async (req, res) => {
     try {
-      let data = req.body;
+      const data = req.body;
       const item = await OrderHistory.create(data);
-      let message = 'Create success.';
+      const message = 'Create success.';
       res.ok({ message, data: { item } } );
     } catch (e) {
       res.serverError(e);
@@ -68,7 +68,7 @@ module.exports = {
     try {
       const { id } = req.params;
       const item = await OrderHistory.destroy({ where: { id } });
-      let message = 'Delete success';
+      const message = 'Delete success';
       res.ok({message, data: {item}});
     } catch (e) {
       res.serverError(e);
