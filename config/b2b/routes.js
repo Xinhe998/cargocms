6223ed2534a4eabe05c596a8407bc3b57baf3e7c
KenYa <tikka0910@gmail.com>
2017-01-19 14:19:05 +0800

    後台新增分類管理介面

--- config/b2b/routes.js
+++ config/b2b/routes.js
@@ -15,4 +15,11 @@ module.exports = {
   'get /ship/*': function(req, res, next) {
     res.sendfile(sails.config.appPath + '/react-app-ship/dist/index.html');
   }
+
+  'get /api/admin/category': 'api/admin/CategoryController.find',
+  'get /api/admin/category/:id': 'api/admin/CategoryController.findOne',
+  'post /api/admin/category': 'api/admin/CategoryController.create',
+  'put /api/admin/category/:id': 'api/admin/CategoryController.update',
+  'delete /api/admin/category/:id': 'api/admin/CategoryController.destroy',
+
 };
