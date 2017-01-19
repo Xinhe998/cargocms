072806d2555661299260b293a48f0968ef3d1fb4
KenYa <tikka0910@gmail.com>
2017-01-19 22:33:47 +0800

    OrderController 加入 orderHistory 功能，使用者可查詢自己的訂單歷史與狀態

--- config/b2b/routes.js
+++ config/b2b/routes.js
@@ -7,10 +7,11 @@ module.exports = {
   'get /product': 'b2b/ProductController.index',
 
   'get /order/form': {
-    // view: 'b2b/order/form'
     controller: 'b2b/OrderController',
     action: 'orderForm'
   },
+
+  'get /orderhistory': 'api/OrderController.getOrderHistory',
 
   'get /ship/*': function(req, res, next) {
     res.sendfile(sails.config.appPath + '/react-app-ship/dist/index.html');
