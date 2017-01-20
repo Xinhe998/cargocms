915bd65eeb2314a6c19f775572c345a361c8365d
smlsunxie <smlsun@gmail.com>
2017-01-20 16:16:15 +0800

    新增 sails-util-mvcsloader 使用 sample

---
+++ api/hooks/sails-hook-echo/config/routes.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jaumard on 12/05/2015.
+ */
+console.log("=== load hook routes ===");
+module.exports.routes = {
+    '/test': 'test/ExampleController.test'
+};
---
+++ api/hooks/sails-hook-echo/index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by jaumard on 28/03/2015.
+ */
+
+module.exports = function (sails) {
+    console.log("=== load hook sample ===");
+    var loader = require('sails-util-mvcsloader')(sails);
+
+    loader.configure({
+        policies: __dirname + '/api/policies', // Path to your hook's policies
+        config: __dirname + '/config' // Path to your hook's config
+    });
+
+    return {
+        initialize: function (next) {
+            // Load controllers, models & services from default directories
+            // loader.adapt(function (err) {
+            //     return next(err);
+            // });
+
+
+                loader.adapt({
+                    controllers: __dirname + '/api/controllers', // Path to your hook's controllers
+                    models: __dirname + '/api/models', // Path to your hook's models
+                    services: __dirname + '/api/services' // Path to your hook's services
+                }, function (err) {
+                    return next(err);
+                });
+
+        }
+    };
+};
