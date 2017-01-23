cd86f765909a1d8efacc090fb1a0b38222044db0
smlsunxie <smlsun@gmail.com>
2017-01-23 17:45:29 +0800

    move labfnp test to hook

---
+++ api/hooks/sails-hook-labfnp/test/util/adminAuthHelper.js
@@ -0,0 +1,28 @@
+var sinon = require('sinon');
+
+module.exports = {
+  mockAdmin: async () => {
+    try {
+      let admin = await User.findOne({
+        where: {
+          username: 'admin',
+        }
+        ,include: Role
+      });
+
+      sinon.stub(AuthService, 'getSessionUser', (req) => {
+        return admin.toJSON();
+      });
+    } catch (e) {
+      throw e;
+    }
+  },
+
+  unMockAdmin: async () => {
+    try {
+      AuthService.getSessionUser.restore();
+    } catch (e) {
+      throw e;
+    }
+  }
+}
