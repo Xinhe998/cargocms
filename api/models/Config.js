e2c4a90912f7f54dc2dd7ac6598514cecbc8dacd
FuYaoDe <dan82625@gmail.com>
2017-02-07 15:51:32 +0800

    實作 sync load 功能

--- api/models/Config.js
+++ api/models/Config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     description: {
       type:  Sequelize.STRING,
-      allowNull: false,
+      // allowNull: false,
     },
     value: {
       type:  Sequelize.STRING,
