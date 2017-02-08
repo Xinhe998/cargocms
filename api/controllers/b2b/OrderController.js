89e492a68a468cab3cef6b00511b54764441f0a1
KenYa <tikka0910@gmail.com>
2017-02-08 11:26:54 +0800

    如果未登入進入結帳頁面，重新導到 login 頁面

--- api/controllers/b2b/OrderController.js
+++ api/controllers/b2b/OrderController.js
@@ -1,5 +1,9 @@
 module.exports = {
   orderForm: async (req, res) => {
+    if (!req.session.authenticated){
+      res.redirect('/login');
+    }
+
     const token = await UtilsService.tokenGenerator();
     res.view(
       'b2b/order/form',
