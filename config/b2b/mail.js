9b4eb2ff4d507fb181cbcba3e2ef2334238074c3
KenYa <tikka0910@gmail.com>
2017-02-08 16:47:49 +0800

    b2b mail 增加 產品出貨時，寄送mail temp

--- config/b2b/mail.js
+++ config/b2b/mail.js
@@ -1,4 +1,26 @@
 
 module.exports = {
+  event: {
+    productShipped: {
+      sendBy: 'email',
+      subject: '訂單 %(orderNumber)s 產品出貨',
+      html: `<html><body>
+      <br />Hi %(username)s:
+
+      <br />親愛的顧客，您所訂購的產品
+      <br />%(productName)s
+      <br />已出貨
+      <br />
+      <br />訂單編號為： %(orderNumber)s
+      <br />聯繫姓名： %(shippingName)s
+      <br />聯繫電話： %(phone)s
+      <br />聯繫地址： %(address)s
+      <br />備註： %(note)s
+      <br />
+      <br />
+      <br />From %(storeName)s
+      </body></html>`
+    },
+  }
 
 }
