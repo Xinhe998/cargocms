5840b6142ae6ea06fb74a350a28c88cd180fb35b
KenYa <tikka0910@gmail.com>
2017-02-09 13:44:34 +0800

    寄送產品出貨通知，補齊寄送內容

--- api/services/MessageService.js
+++ api/services/MessageService.js
@@ -354,10 +354,26 @@ module.exports = {
   },
 
   orderProductShipped: async(result = {
-    orderNumber, productName, shippingName, phone, address, email
+    orderNumber, productName, shippingName, username, telephone, address, email
   }) => {
     try {
       let productShipped = sails.config.mail.templete.event.productShipped;
+      let mailSendConfig = {...productShipped, to: result.email}
+      mailSendConfig.subject = sprintf(mailSendConfig.subject, {
+        orderNumber: result.orderNumber
+      });
+      mailSendConfig.html = sprintf(mailSendConfig.html, {
+        username: result.username,
+        productName: result.productName,
+        orderNumber: result.orderNumber,
+        shippingName: result.shippingName,
+        phone: result.telephone,
+        address: result.address,
+        storeName: sails.config.storeName,
+      });
+      mailSendConfig.type = 'deliveryConfirm';
+      return mailSendConfig;
+
     } catch (e) {
       throw e;
     }
