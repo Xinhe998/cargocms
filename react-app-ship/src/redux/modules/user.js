97205c1507bbe6827ab339fdf80cfa91e5438ea5
KenYa <tikka0910@gmail.com>
2017-02-15 15:53:43 +0800

    react-ship 取得 currentUser 修改判斷是否「登入」以及「是否為供應商」

--- react-app-ship/src/redux/modules/user.js
+++ react-app-ship/src/redux/modules/user.js
@@ -26,20 +26,27 @@ export function fetchCurrentUserData() {
     const fetchResult = await getData(API_GET_CURRENT_USER);
     // success
     if (fetchResult.status) {
-      const rolesArray = fetchResult.data.currentUser.rolesArray;
-      let isSupplier = false;
-      for (const role of rolesArray) {
-        if (role === 'supplier') {
-          isSupplier = true;
+      if (fetchResult.data.success) {
+        const rolesArray = fetchResult.data.data.currentUser.rolesArray;
+        let isSupplier = false;
+        for (const role of rolesArray) {
+          if (role === 'supplier' || role === 'admin') {
+            isSupplier = true;
+          }
         }
-      }
-      isSupplier = isSupplier && fetchResult.data.currentUser.SupplierId;
-      if (isSupplier) {
-        dispatch(deliverCurrentUserData(fetchResult.data.currentUser));
+        isSupplier = isSupplier && fetchResult.data.data.currentUser.SupplierId;
+        if (isSupplier) {
+          dispatch(deliverCurrentUserData(fetchResult.data.data.currentUser));
+        } else {
+          dispatch(handleResponse({
+            response: { status: 401 },
+            message: '只有供應商才能登入供應商後台！',
+          }));
+        }
       } else {
         dispatch(handleResponse({
-          response: { status: 403 },
-          message: '只有供應商才能登入供應商後台！',
+          response: { status: 401 },
+          message: '請先登入帳號！',
         }));
       }
     // error
