244f78b93b4ebec40b90994c7cafddd211f166d8
FuYaoDe <dan82625@gmail.com>
2017-02-20 18:32:09 +0800

    fix should

--- test/unit/controllers/ServerPeckerController.spec.js
+++ test/unit/controllers/ServerPeckerController.spec.js
@@ -22,16 +22,16 @@ describe('about SearchPecker Controller operation.', function() {
       .put(`/api/admin/crawler/rank/${searchPeckerId}`)
       .send(updateData);
       res.body.should.be.Object;
-      res.status.shoule.be.eq(200);
-      res.body.data.keywords.shoule.be.eq(updateData.keywords);
-      res.body.data.crawlerAgent.shoule.be.eq(updateData.crawlerAgent);
-      res.body.data.reportHtml.shoule.be.eq(updateData.reportHtml);
-      res.body.data.reportImage.shoule.be.eq(updateData.reportImage);
-      res.body.data.pageNo.shoule.be.eq(updateData.pageNo);
-      res.body.data.pageNoPrev.shoule.be.eq(updateData.pageNoPrev);
-      res.body.data.pageNoWarn.shoule.be.eq(updateData.pageNoWarn);
-      res.body.data.targetUrl.shoule.be.eq(updateData.targetUrl);
-      res.body.data.searchEngine.shoule.be.eq(updateData.searchEngine);
+      res.status.should.be.eq(200);
+      res.body.data.keywords.should.be.eq(updateData.keywords);
+      res.body.data.crawlerAgent.should.be.eq(updateData.crawlerAgent);
+      res.body.data.reportHtml.should.be.eq(updateData.reportHtml);
+      res.body.data.reportImage.should.be.eq(updateData.reportImage);
+      res.body.data.pageNo.should.be.eq(updateData.pageNo);
+      res.body.data.pageNoPrev.should.be.eq(updateData.pageNoPrev);
+      res.body.data.pageNoWarn.should.be.eq(updateData.pageNoWarn);
+      res.body.data.targetUrl.should.be.eq(updateData.targetUrl);
+      res.body.data.searchEngine.should.be.eq(updateData.searchEngine);
       done();
     } catch (e) {
       done(e);
