47087107e48c4a6e0e2aa7b7ed23cb07d86c6764
KenYa <tikka0910@gmail.com>
2017-02-21 18:25:15 +0800

    將驗證 email 是否有效的 policies 分離到 validEmail

--- api/policies/validEmail.js
+++ api/policies/validEmail.js
@@ -12,18 +12,16 @@ module.exports = async function(req, res, next) {
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
   const user = AuthService.getSessionUser(req);
-  console.log("req.session", user);
-  if (sails.config.offAuth || user) {
-    // const noEmail = !user.email;
-    // if (noEmail || user.email === '') {
-    //   sails.log.warn('使用者登入沒有 Email');
-    //   req.flash('info', '請補齊 Email 資料');
-    //   return res.redirect('/edit/me');
-    // }
-    return next();
+
+  if (sails.config.verificationEmail && user.verificationEmailToken) {
+    const modelUser = await User.findById(user.id);
+
+    if (modelUser.verificationEmailToken) {
+      req.flash('info', '請先驗證完您的 Email 才能使用此功能');
+      return res.redirect('/edit/me');
+    }
   }
 
-  // User is not allowed
-  // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  return next();
+
 };
