f13a7b2778fac5429ac6d1978f127a580659c133
KenYa <tikka0910@gmail.com>
2017-03-01 12:52:18 +0800

    加入建立訂單後的 mail

--- config/b2b/mail.js
+++ config/b2b/mail.js
@@ -23,6 +23,28 @@ module.exports = {
         <br />From %(storeName)s
         </body></html>`
       },
+      orderCreated: {
+        sendBy: 'email',
+        subject: '訂單 %(orderSerialNumber)s 建立完成',
+        html: `<html><body>
+        <br />Hi %(username)s:
+
+        <br />感謝你的訂購，你所購買的產品
+        <br />
+        <br />%(productName)s
+        <br />
+        <br />已訂購完成
+        <br />
+        <br />訂單編號為： %(orderSerialNumber)s
+        <br />收件者為： %(shipmentUsername)s
+        <br />收件者電話： %(phone)s
+        <br />收件地址為： %(shipmentAddress)s
+        <br />備註： %(note)s
+        <br />
+        <br />
+        <br />From %(storeName)s
+        </body></html>`
+      },
     }
   }
 }
