fd820cec8e4902951a1c310cf57428a0a5cc7303
Kuyen <iamcxa@gmail.com>
2017-03-02 17:10:59 +0800

    update import mapping

--- config/b2b/import.js
+++ config/b2b/import.js
@@ -3,18 +3,18 @@ module.exports = {
     import: {
         tables: [{
             "targetModel": "Product",
-            "jsonfile": "../../import/b2b/product/output.json",
+            "jsonfile": "../../import/b2b/product.json",
             "mapping": [
                 { "from": "name", "to": "productName" },
                 { "from": "source", "to": "supplierName" },
                 { "from": "spec", "to": "weight" },
-                { "from": "typeEng", "to": "categoryEng" },
-                { "from": "type", "to": "category" },
+                // { "from": "categoryEng", "to": "categoryEng" },
+                { "from": "category", "to": "category" },
                 { "from": "image", "to": "image" },
                 { "from": "desc", "to": "description" },
-                { "from": "tel", "to": "tel" },
-                { "from": "fax", "to": "fax" },
-                { "from": "address", "to": "address" }
+                { "from": "source-tel", "to": "tel" },
+                { "from": "source-fax", "to": "fax" },
+                { "from": "source-addr", "to": "address" }
             ]
         }],
     }
