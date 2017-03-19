38d2d5c4c47b25ac8ef89f12439df8274eb21ad7
Vagrant <xup6u06m4@gmail.com>
2017-03-20 02:10:55 +0800

    review 後的修改，完成一些小細節

--- api/controllers/admin/RoleDetailController.js
+++ api/controllers/admin/RoleDetailController.js
@@ -1,28 +1,46 @@
+var menuItems = {}, roleid = {};
+(async function() {
+  menuItems = await MenuItem.findAllWithSubMenu();
+  roleid = await Role.findAll();
+})();
+
 module.exports = {
   index: async (req, res) => {
     res.ok({
       view: true,
       serverSidePaging: true,
-      layout: 'admin/default/index'
+      layout: 'admin/default/index',
+      name: ["READ_WRITE", "READ", "CREATE", "UPDATE", "DELETE"],
+      roleid: roleid,
+      menuItems: menuItems
     });
   },
   create: async (req, res) => {
     res.ok({
       view: true,
-      layout: 'admin/default/create'
+      layout: 'admin/default/create',
+      name: ["READ_WRITE", "READ", "CREATE", "UPDATE", "DELETE"],
+      roleid: roleid,
+      menuItems: menuItems
     });
   },
   edit: async (req, res) => {
     res.ok({
       view: true,
-      layout: 'admin/default/edit'
+      layout: 'admin/default/edit',
+      name: ["READ_WRITE", "READ", "CREATE", "UPDATE", "DELETE"],
+      roleid: roleid,
+      menuItems: menuItems
     });
 
   },
   show: async (req, res) => {
     res.ok({
       view: true,
-      layout: 'admin/default/show'
+      layout: 'admin/default/show',
+      name: ["READ_WRITE", "READ", "CREATE", "UPDATE", "DELETE"],
+      roleid: roleid,
+      menuItems: menuItems
     });
   },
 }
