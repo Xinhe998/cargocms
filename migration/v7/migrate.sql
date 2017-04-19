233f905c27527967844ca805f40a7f6bae2ac942
KenYa <tikka0910@gmail.com>
2017-04-19 10:34:39 +0800

    增加 Product & Supplier 表格變更 Migration

---
+++ migration/v7/migrate.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `ProductSupplier` (
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `ProductId` int(11) NOT NULL,
+  `SupplierId` int(11) NOT NULL,
+  PRIMARY KEY (`ProductId`,`SupplierId`),
+  KEY `SupplierId` (`SupplierId`),
+  CONSTRAINT `productsupplier_ibfk_1` FOREIGN KEY (`ProductId`) REFERENCES `Products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `productsupplier_ibfk_2` FOREIGN KEY (`SupplierId`) REFERENCES `Suppliers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `Products` DROP `SupplierId`;
